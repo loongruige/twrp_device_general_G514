c26a1540cd9e3fa7a6fc5d7de94e510fda34c7fc
Efe Timur Turan <tasalxntn@gmail.com>
2024-05-26 15:13:32 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_G514-user
-add_lunch_combo omni_G514-userdebug
-add_lunch_combo omni_G514-eng
+add_lunch_combo twrp_G514-user
+add_lunch_combo twrp_G514-userdebug
+add_lunch_combo twrp_G514-eng
